1d0359c498331607402bea2490b6eef2c8e21f62
Rossastoria <Rossastoria@users.noreply.github.com>
2016-07-27 18:03:59 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -62,4 +62,3 @@ write.table(tidyavg, ".tidyavg.txt")
   ## ----- 'tidyavg' is a dataframe of 180 obs and 81 variable, from which can be read
   ## ----- the subject's identification number, the motion subject was taking at the time of obs, and 
   ## ----- the mean of the feature for that subject doing that motion.
-}
